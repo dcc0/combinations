223e928ad3420841701f11d6e8af5c66158e365a
dcc0 <dcc0@yandex.ru>
2018-06-02 21:52:45 +0300

    Update combinations.php

--- .gitignore/combinations.php
+++ .gitignore/combinations.php
@@ -7,8 +7,10 @@ $a = array(
 	4,
 	5
 );
+
 $k = 2; /*Set a value*/
 $n = 5; /*Set a value*/
+
 
 /*We cut it for two arrays:
 * in B - elements with element on a position K and
@@ -22,6 +24,7 @@ print_r($b);
 
 while (1)
 	{
+	
 	/*Here we search an element in array C greater than K in array B.
 	* Ищем элемент в массиве С, больше на единицу, чем
 	* элемент в B на позиции K*/
@@ -29,6 +32,7 @@ while (1)
 	if ($more_per_unit !== false)
 		{
 
+		
 		/*Found. Change. Нашли.
 		* Транспортируем единицу*/
 		$c[$more_per_unit]-= 1;
@@ -36,6 +40,7 @@ while (1)
 		print_r($b);
 		}
 
+	
 	/*Если последний элемент в массива B равен n
 	* - старшему значению, то цикл поиска элемента слева от К,
 	* для которого в массиве C есть больший на единицу.*/
@@ -43,17 +48,20 @@ while (1)
 		{
 		$i = $k - 1;
 
+		
 		/*Просмотр массива справа налево.
 		* Looking greater element (from right to left).*/
 		while ($i >= 0)
 			{
 
+			
 			/*Если дошли до 0 индекса и таких элементов нет,
 			* то алгоритм завершается.
 			* Index 0 and we didn't find the element.
 			* This condition stops the algorithm.*/
 			if ($i == 0 && $b[$i] == $n - $k + 1) break 2;
 
+			
 			/*Поиск элемента для увеличения.
 			* Looking for an element to increase it.*/
 			$more_per_unit = array_search($b[$i] + 1, $c);
@@ -62,6 +70,7 @@ while (1)
 				$c[$more_per_unit] = $c[$more_per_unit] - 1;
 				$b[$i] = $b[$i] + 1;
 
+				
 				/*Перенос значений в С. Заполнение  массива B до K*.
 				*Here we transport elements to C and fill array B till K.*/
 				for ($j = $i; $j != $k - 1; $j++)
@@ -70,6 +79,7 @@ while (1)
 					$b[$j + 1] = $b[$j] + 1;
 					}
 
+				
 				/*Удаление повторяющихся значений из C.*
 				* Here we remove duplicates out of C.*/
 				$c = array_diff($c, $b);
